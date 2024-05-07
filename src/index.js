10ebf0729805fd1f991049b2ceebfea75e1743d2
Isabelah Mwandawiro <isabelahhadia@gmail.com>
2024-05-07 07:16:40 +0300

    update styles.css
    added some classes

--- src/index.js
+++ src/index.js
@@ -86,12 +86,12 @@ function displayForecast(response) {
     if (index < 5) {
       forecastHtml =
         forecastHtml +
-        `<div class="weather-forecast-day>
+        `<div class="weather-forecast-day">
           <div class= "weather-forecast-date">${formatDay(day.time)}</div>
           <img src="${day.condition.icon_url}" class= "weather-forecast-icon" />
           <div class= "weather-forecast-temperatures">
            <div class= "weather-forecast-temperature">
-             <strong>${Math.round(day.temperature.maximum)}&deg;</strong>
+             <strong>${Math.round(day.temperature.maximum *9)}&deg;</strong>
              </div>
              <div class= "weather-forecast-temperature">${Math.round(day.temperature.minimum)}&deg;</div>
              </div>
