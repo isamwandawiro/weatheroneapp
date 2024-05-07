f82a83692986e8894cec60f7efaf4cdccba5f1b5
Isabelah Mwandawiro <isabelahhadia@gmail.com>
2024-05-07 07:22:09 +0300

    Update index.js
    updated line 94

--- src/index.js
+++ src/index.js
@@ -91,7 +91,7 @@ function displayForecast(response) {
           <img src="${day.condition.icon_url}" class= "weather-forecast-icon" />
           <div class= "weather-forecast-temperatures">
            <div class= "weather-forecast-temperature">
-             <strong>${Math.round(day.temperature.maximum *9)}&deg;</strong>
+             <strong>${Math.round(day.temperature.maximum)}&deg;</strong>
              </div>
              <div class= "weather-forecast-temperature">${Math.round(day.temperature.minimum)}&deg;</div>
              </div>
